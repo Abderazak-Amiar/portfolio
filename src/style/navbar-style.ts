464b4242abf7a9a135c3a2559ad4c71b142f95a3
Abderazak-Amiar <amiar.abderazak@gmail.com>
2024-07-01 10:12:02 +0100

    style(component): Add style to the navigation bar component and its related children

---
+++ src/style/navbar-style.ts
@@ -0,0 +1,78 @@
+export const GridLogoStyle = {
+  display: 'flex',
+  justifyContent: {
+    xs: 'start',
+    sm: 'start',
+    md: 'center',
+    lg: 'center',
+    xl: 'center',
+  },
+};
+
+export const GridNavMenuStyle = {
+  display: {
+    xs: 'none',
+    sm: 'none',
+    md: 'block',
+    lg: 'block',
+    xl: 'block',
+  },
+};
+export const GridDownloadCvStyle = {
+  display: 'flex',
+  justifyContent: {
+    xs: 'end',
+    sm: 'end',
+    md: 'center',
+    lg: 'center',
+    xl: 'center',
+  },
+};
+
+export const BtnDownloadCvStyle = {
+  textTransform: 'capitalize',
+  display: {
+    xs: 'none',
+    sm: 'none',
+    md: 'block',
+    lg: 'block',
+    xl: 'block',
+  },
+};
+export const HamburgerMenuIconStyle = {
+  textAlign: 'end',
+  display: {
+    xs: 'block',
+    sm: 'block',
+    md: 'none',
+    lg: 'none',
+    xl: 'none',
+  },
+};
+export const PopoverMenuStyle = {
+  backgroundColor: 'rgba(0, 0, 0, 0.4)',
+  backdropFilter: 'blur(4px) opacity(97%)',
+  margin: 0,
+  padding: 0,
+  display: {
+    xs: 'block',
+    sm: 'block',
+    md: 'none',
+    lg: 'none',
+    xl: 'none',
+  },
+};
+export const PopoverGridLogoStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+};
+export const PopoverGridLinkMenuStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+};
+export const PopoverGridDownloadCvStyle = {
+  textTransform: 'capitalize',
+  width: '150px',
+};
